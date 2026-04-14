993797e11a6bd254aaca58d3966d3bc9f1222f96
althea-aeryn-delacruz <delacruz.althea.aeryn@gmail.com>
2026-04-14 08:24:14 +0800

    Add group expense now usable (needs improved logi)

--- app/src/main/java/com/example/fairshare/ui/groups/GroupLobbyActivity.java
+++ app/src/main/java/com/example/fairshare/ui/groups/GroupLobbyActivity.java
@@ -13,6 +13,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
